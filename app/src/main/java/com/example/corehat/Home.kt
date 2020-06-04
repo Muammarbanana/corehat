966160280e0b025e21d42e161a56d53b334c1b3a
Muammarbanana <muammarfanani2@gmail.com>
2020-06-04 12:38:58 +0700

    get counselor's photo

--- app/src/main/java/com/example/corehat/Home.kt
+++ app/src/main/java/com/example/corehat/Home.kt
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_home.*
 
 class Home : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var ref: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,5 +28,25 @@ class Home : AppCompatActivity() {
             intent = intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
             startActivity(intent)
         }
+
+        getPhoto()
+    }
+
+    private fun getPhoto() {
+        ref = FirebaseDatabase.getInstance().getReference("konselor")
+        ref.orderByKey().equalTo(auth.currentUser?.uid!!).addValueEventListener(object: ValueEventListener{
+            override fun onCancelled(p0: DatabaseError) {
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                if (p0.exists()) {
+                    for (h in p0.children) {
+                        val photo = h.child("url_foto").value.toString()
+                        Picasso.get().load(photo).into(conselPhoto)
+                    }
+                }
+            }
+
+        })
     }
 }
